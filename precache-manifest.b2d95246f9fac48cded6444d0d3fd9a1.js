5c99297a1944e8881d9916bddbfefc83b5411587
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2020-08-12 23:24:05 +0530

    new build with less code

---
+++ precache-manifest.b2d95246f9fac48cded6444d0d3fd9a1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f092a65ad9ff99d585ba1f6fbe41e18",
+    "url": "/pst-car-booking-react/index.html"
+  },
+  {
+    "revision": "2604cf84dcd155aa31f0",
+    "url": "/pst-car-booking-react/static/css/2.a6e45c16.chunk.css"
+  },
+  {
+    "revision": "8f909ed6cc6610f2a94d",
+    "url": "/pst-car-booking-react/static/css/main.ad4d2d47.chunk.css"
+  },
+  {
+    "revision": "2604cf84dcd155aa31f0",
+    "url": "/pst-car-booking-react/static/js/2.2674c63d.chunk.js"
+  },
+  {
+    "revision": "fb5619fe73a36153c26d182f1246b352",
+    "url": "/pst-car-booking-react/static/js/2.2674c63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f909ed6cc6610f2a94d",
+    "url": "/pst-car-booking-react/static/js/main.423bb44f.chunk.js"
+  },
+  {
+    "revision": "d505ce18cfdd4f0dd66e",
+    "url": "/pst-car-booking-react/static/js/runtime-main.3d9a7e25.js"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/pst-car-booking-react/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "d3d12ddce1d5822c4839738def47ef70",
+    "url": "/pst-car-booking-react/static/media/audi.d3d12ddc.png"
+  },
+  {
+    "revision": "1eb31acb54e247e90009ca6174d1032d",
+    "url": "/pst-car-booking-react/static/media/bg.1eb31acb.jpg"
+  },
+  {
+    "revision": "07abb74354a06e375fd0a3c2293222d9",
+    "url": "/pst-car-booking-react/static/media/car-group.07abb743.png"
+  },
+  {
+    "revision": "e3dd15f8ad0bada80faa62d925076c0d",
+    "url": "/pst-car-booking-react/static/media/daily-wages-driver-crop.e3dd15f8.jpg"
+  },
+  {
+    "revision": "5d0ed35a5dac4157447adab8f7649e2b",
+    "url": "/pst-car-booking-react/static/media/local-tour.5d0ed35a.jpg"
+  },
+  {
+    "revision": "40b80ea9551dcb075a8d6eff760783dd",
+    "url": "/pst-car-booking-react/static/media/long-tour.40b80ea9.jpg"
+  },
+  {
+    "revision": "7ce34c5b4fdd567d60ef6027218f93d6",
+    "url": "/pst-car-booking-react/static/media/permanent-crop.7ce34c5b.jpg"
+  }
+]);
